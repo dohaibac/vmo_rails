ab74ba1649c4d35758d63967b4cb78147c2e943b
BacDo <bacdo@BacDos-MBP.net.fpt>
2019-12-29 21:27:12 +0700

    update donate function

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -6,35 +6,30 @@ class WebsiteController < ApplicationController
   def donate
     charity = params[:charity] == "random" ? Charity.find(Charity.pluck(:id).sample) : Charity.find_by_id(params[:charity])
 
-    if charity && params[:omise_token].present?
-      unless params[:amount].blank? || params[:amount].to_i <= 20
-        if Rails.env.test?
-          charge = OpenStruct.new({
-            amount: params[:amount].to_i * 100,
-            paid: (params[:amount].to_i != 999),
-          })
-        else
-          charge = Omise::Charge.create({
-            amount: params[:amount].to_i * 100,
-            currency: "THB",
-            card: params[:omise_token],
-            description: "Donation to #{charity.name} [#{charity.id}]",
-          })
-        end
-
-        if charge.paid
-          charity.credit_amount(charge.amount)
-          flash.notice = t(".success")
-          redirect_to root_path
-        else
-          donate_fail()
-        end
+    if charity && params[:omise_token].present? && !(params[:amount].blank? || params[:amount].to_i <= 20)
+      if Rails.env.test?
+        charge = OpenStruct.new({
+          amount: params[:amount].to_i * 100,
+          paid: (params[:amount].to_i != 999),
+        })
       else
-        @token = retrieve_token(params[:omise_token])
-        flash.now.alert = t(".failure")
-        render :index
-        return
+        charge = Omise::Charge.create({
+          amount: params[:amount].to_i * 100,
+          currency: "THB",
+          card: params[:omise_token],
+          description: "Donation to #{charity.name} [#{charity.id}]",
+        })
       end
+
+      if charge.paid
+        charity.credit_amount(charge.amount)
+        flash.notice = t(".success")
+        redirect_to root_path
+      else
+        donate_fail()
+      end
+    elsif (params[:amount].blank? || params[:amount].to_i <= 20)
+      donate_fail(retrieve_token(params[:omise_token]))
     else
       donate_fail()
     end
@@ -60,8 +55,8 @@ class WebsiteController < ApplicationController
     end
   end
 
-  def donate_fail()
-    @token = nil
+  def donate_fail(token = nil)
+    @token = token
     flash.now.alert = t(".failure")
     render :index
     return
